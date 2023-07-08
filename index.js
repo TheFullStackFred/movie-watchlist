5ca071725cd8c4ee2c6b15cdea6305989264f636
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-07-08 16:01:15 +0200

    Handle error in fetch

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const searchInput = document.getElementById('search__input')
 const searchBtn = document.getElementById('search__btn')
 
 searchBtn.addEventListener('click', searchMovie)
+
 async function searchMovie(e) {
   e.preventDefault()
-  const res = await fetch(
-    ` http://www.omdbapi.com/?s=${searchInput.value}&apikey=${API_KEY}`
-  )
-  const data = await res.json()
-  console.log(data)
+
+  try {
+    const res = await fetch(
+      ` http://www.ombapi.com/?s=${searchInput.value}&apikey=${API_KEY}`
+    )
+    const data = await res.json()
+    console.log(data)
+  } catch (err) {
+    console.error(err)
+  }
 }
