6ebce3f8160146b343d428731411b96081e7ba55
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-07-08 15:57:33 +0200

    Set up fetch

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+import API_KEY from './apiKey.js'
+
 const searchInput = document.getElementById('search__input')
 const searchBtn = document.getElementById('search__btn')
 
 searchBtn.addEventListener('click', searchMovie)
-function searchMovie(e) {
+async function searchMovie(e) {
   e.preventDefault()
-  console.log(searchInput.value)
+  const res = await fetch(
+    ` http://www.omdbapi.com/?s=${searchInput.value}&apikey=${API_KEY}`
+  )
+  const data = await res.json()
+  console.log(data)
 }
